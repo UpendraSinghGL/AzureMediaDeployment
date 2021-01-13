12f282c0a7de9fec9716ff9370a6f3315c348147
v-sosah <43109112+v-sosah@users.noreply.github.com>
2021-01-14 00:49:12 +0530

    add changes to solution src

---
+++ cds/src/WebResources/media_durinutility.js
@@ -0,0 +1,7 @@
+(function (DurinMedia) {
+    var Utility = {};
+    DurinMedia.Utility = Utility;
+    Utility.displayIconInView = function (a, b, c) {
+        return [JSON.parse(a).media_logo_url_Value];
+    }
+}(window.DurinMedia = window.DurinMedia || {}))
