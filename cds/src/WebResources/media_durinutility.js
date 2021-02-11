f75d65878c96e53aec523e1ba5d9fcfd6daafc85
v-sosah <43109112+v-sosah@users.noreply.github.com>
2021-02-11 17:16:42 +0530

    nexis, vendor related changes

--- cds/src/WebResources/media_durinutility.js
+++ cds/src/WebResources/media_durinutility.js
@@ -4,4 +4,12 @@
     Utility.displayIconInView = function (a, b, c) {
         return [JSON.parse(a).media_logo_url_Value];
     }
+
+    Utility.NavigateTo = function (selectedid, entity, attribute) {
+        fetch(`/api/data/v9.1/${entity}/${attribute}(${selectedid.replace(/[{}]/g, '')})`)
+            .then(res => res.json())
+            .then(data => {
+                location.href = data.value;
+            })
+    }
 }(window.DurinMedia = window.DurinMedia || {}))
